f6e71eabd140ab7a3c5167af3c0edf11b2a3f95c
DBJD-CR <3405689090@qq.com>
2026-08-11 04:58:43 +0800

    refactor(utils): 抽取中国省级行政区划公共工具并消除重复定义
    - weather_region_resolver、stats_event_support_service 删除本地重复的
      CHINA_PROVINCES 定义，改引 utils.china_regions 公共常量；
    - weather_station_query_service 复用 strip_province_prefix 解析「省+站名」前缀
      与 province_short 展示简称，删除本地 _COMMON_PROVINCES /
      _PROVINCE_FULL_TO_SHORT 等重复实现；
    - 该文件同步改用 utils.text_format_utils 的 norm_value / MISSING_VALUE
      格式化数值，删除本地 _norm_value 重复实现。

--- core/services/geo/weather_region_resolver.py
+++ core/services/geo/weather_region_resolver.py
@@ -19,43 +19,7 @@ import aiohttp
 
 from astrbot.api import logger
 
-# 中国 34 个省级行政区划简称与全名关键字定义列表
-CHINA_PROVINCES = [
-    "北京",
-    "天津",
-    "上海",
-    "重庆",
-    "河北",
-    "山西",
-    "辽宁",
-    "吉林",
-    "黑龙江",
-    "江苏",
-    "浙江",
-    "安徽",
-    "福建",
-    "江西",
-    "山东",
-    "河南",
-    "湖北",
-    "湖南",
-    "广东",
-    "海南",
-    "四川",
-    "贵州",
-    "云南",
-    "陕西",
-    "甘肃",
-    "青海",
-    "台湾",
-    "内蒙古",
-    "广西",
-    "西藏",
-    "宁夏",
-    "新疆",
-    "香港",
-    "澳门",
-]
+from ....utils.china_regions import CHINA_PROVINCES
 
 # ResAPI 行政区划搜索接口地址
 _RESAPI_REGIONS_SEARCH_URL = "https://www.resapi.cn/v1/regions/search"
