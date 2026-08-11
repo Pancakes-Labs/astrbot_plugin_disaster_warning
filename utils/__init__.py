3f5cdb5e467c9d416a214d01e93eed4c4bd6c921
DBJD-CR <3405689090@qq.com>
2026-08-11 20:54:11 +0800

    chore: 统一导出视觉指示器模块

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,7 @@
 """
 插件辅助工具子包。
-包含 IP 物理定位（GeoIP）、地图瓦片 URL 映射、时区转换格式化、emoji过滤、版本识别探测以及烈度制式转换等基础通用库。
+包含 IP 物理定位（GeoIP）、地图瓦片 URL 映射、时区转换格式化、emoji过滤、
+统一等级视觉指示器、版本识别探测以及烈度制式转换等基础通用库。
 """
 
 from .converters import ScaleConverter
@@ -14,6 +15,14 @@ from .emoji_filter import (
 )
 from .geolocation import close_geoip_session, fetch_location_from_ip, get_geoip_session
 from .map_tile_sources import get_tile_url, get_tile_url_js, normalize_map_source
+from .severity_emoji import (
+    SEVERITY_INDICATOR_EMOJIS,
+    aqi_level_emoji,
+    cn_tsunami_level_emoji,
+    intensity_level_emoji,
+    rank_level_emoji,
+    typhoon_level_emoji,
+)
 from .time_converter import TimeConverter
 from .version import get_astrbot_version, get_astrbot_version_info, get_plugin_version
 
@@ -31,6 +40,12 @@ __all__ = [
     "get_tile_url",
     "get_tile_url_js",
     "normalize_map_source",
+    "SEVERITY_INDICATOR_EMOJIS",
+    "aqi_level_emoji",
+    "cn_tsunami_level_emoji",
+    "intensity_level_emoji",
+    "rank_level_emoji",
+    "typhoon_level_emoji",
     "TimeConverter",
     "get_astrbot_version",
     "get_astrbot_version_info",
